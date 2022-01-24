718186347d0ee13588eda50e783a1fdde36e40cf
Inderpreet Singh <31967757+inderSing@users.noreply.github.com>
2022-01-24 13:42:22 -0800

    Time: 0 ms (100.00%), Space: 41 MB (6.39%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int pivot;
+        int left = 0, right = nums.length-1;
+        while( left <= right) {
+            pivot = left + ( right - left)/2;
+            if( nums[pivot] == target) return pivot;
+            if( target < nums[pivot]) right = pivot-1;
+            else left = pivot + 1;
+        }
+        return left;
+    }
+}
