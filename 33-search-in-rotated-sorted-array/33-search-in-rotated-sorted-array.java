c6a409350d5b61f4de6fdf8caaea2232759b7bd8
Inderpreet Singh <31967757+inderSing@users.noreply.github.com>
2022-01-25 09:41:22 -0800

    Time: 0 ms (100.00%), Space: 39.5 MB (26.48%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,74 +1,33 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if( nums[0] == target) {
-            return 0;
-        }
         if( nums.length == 1 && nums[0] == target) {
             return 0;
         }
         if( nums.length == 1 && nums[0] != target) {
             return -1;
         }
-        int i = findsmallest(nums, 0, nums.length-1);
-        System.out.println(i);
-        if( i == 0 ) {
-            return binarySearch( nums, 0, nums.length - 1, target);
-        }
-        if( nums[0] >= target && nums[i] <= target) {
-            return binarySearch(nums, i, nums.length-1, target);
-        } else {
-            System.out.println("Over here");
-            return binarySearch(nums, 0, i-1, target);
-        }
-    }
-    
-    public int binarySearch(int[] nums, int start, int end, int target) {
-        if( end <= start ) {
-            if( nums[start] == target) {
-                return start;
-            } else {
-                System.out.println("Over here");
-                return -1;
-            }
-        }
+        int start = 0;
+        int end = nums.length - 1;
         int pivot;
-        while( start <= end) {
-            pivot = start + ((end-start)/2);
-            //System.out.println(pivot);
-            if( nums[pivot] == target) {
-                System.out.println("Over here");
-                return pivot;
-            }
-            if( nums[pivot] > target) {
-                end = pivot - 1;
-            } else {
-                start = pivot + 1;
-            }
-        }
-        //System.out.println("Over here");
-        return -1;
-    }
-    
-    public int findsmallest(int[] nums, int start, int end) {
-        if(nums[start] < nums[end]) {
-            return 0;
-        }
-        int pivot;// = start + ( end - start)/2;
         while( start <= end ) {
-            pivot = start + ( end - start)/2;
-            if( nums[pivot] > nums[pivot+1]) {
-                return (pivot+1);
-            }
-            if( nums[pivot] < nums[pivot-1] && nums[pivot] < nums[pivot+1]) {
+            pivot = start + (end - start)/2;
+            if( nums[pivot] == target) {
                 return pivot;
             }
-            if( nums[pivot] > nums[start] ) {
-                start = pivot + 1;
+            else if( nums[pivot] >= nums[start] ) {
+                if(target < nums[pivot] && target >= nums[start]) {
+                    end = pivot - 1;
+                } else {
+                    start = pivot + 1;
+                }
             } else {
-                end = pivot - 1;
+                if( target >= nums[pivot] && target <= nums[end]) {
+                    start = pivot + 1;
+                } else {
+                    end = pivot - 1;
+                }
             }
         }
-        System.out.println("Over here");
-        return 0;
+        return -1;
     }
 }
