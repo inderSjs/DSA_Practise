9ed891ccee6d58b4005d91007850f175f3beeb72
Inderpreet Singh <31967757+inderSing@users.noreply.github.com>
2022-02-03 11:25:16 -0800

    Time: 57 ms (37.12%), Space: 70.9 MB (13.43%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,61 +1,46 @@
 class Solution {
-    boolean[][] t;
-    
+    boolean[][] dp;
+    int[][] t;
     public boolean canPartition(int[] nums) {
+        //recursive solution
         int sum = 0;
-        for(int i = 0; i < nums.length; i++) {
+        int N = nums.length;
+        for(int i = 0; i < N; i++) {
             sum = sum + nums[i];
         }
         if( sum % 2 != 0) {
             return false;
         }
         sum = sum/2;
-        t = new boolean[nums.length+1][sum+1];
-        for(int i = 0; i <= nums.length; i++) {
+        dp = new boolean[N][sum+1];
+        t = new int[N][sum+1];
+        for(int i = 0; i < N; i++) {
             for(int j = 0; j <= sum; j++) {
-                if( i == 0 ) {
-                    t[i][j] = false;
-                }
-                if( j == 0 ) {
-                    t[i][j] = true;
-                }
+                t[i][j] = -100;
             }
         }
-        t[0][0] = true;
-        for(int i = 1; i <= nums.length; i++) {
-            for(int j = 1; j <= sum; j++) {
-                if( nums[i-1] <= j ) {
-                    t[i][j] = t[i-1][j - nums[i-1]] || t[i-1][j];
-                } else {
-                    t[i][j] = t[i-1][j];
-                }
-            }
-        }
-        return t[nums.length][sum];
+        return helper(nums, sum, N-1);
     }
- /*   
-    public boolean helper(int[] nums, int n, int target) {
+    
+    public boolean helper(int[] arr, int target, int n) {
         if( target == 0 ) {
             return true;
         }
-        if( n < 0 || target < 0 ) {
+        if( n < 0 ) {
             return false;
         }
         if( t[n][target] != -100) {
-            if( t[n][target] == 1 ) {
-                return true;
-            } else {
-                return false;
-            }
+            //t[n][target] = 1;
+            return dp[n][target];
         }
-        boolean include = helper(nums, n-1, target - nums[n]);
-        boolean exclude = helper(nums, n-1, target);
-        boolean x = (include || exclude);
-        if( x ) {
+        if( arr[n] <= target ) {
             t[n][target] = 1;
+            dp[n][target] = helper(arr, target - arr[n], n-1) || helper(arr, target, n-1);
+            return dp[n][target];
         } else {
-            t[n][target] = 0;
+            t[n][target] = 1;
+            dp[n][target] = helper(arr,target, n-1);
+            return dp[n][target];
         }
-        return x;
-    }  */
+    }
 }
