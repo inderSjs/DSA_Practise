37071454efc240fa9daa4c52c54027a2e548c2b4
Inderpreet Singh <31967757+inderSing@users.noreply.github.com>
2022-02-04 09:43:54 -0800

    Time: 15 ms (37.14%), Space: 55.6 MB (5.56%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,19 +1,6 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        /*int n = intervals.length;
-        for(int i = 0; i < (n-1); i++) {
-            int minIndex = i;
-            for(int j = i+1; j < n; j++) {
-                if( intervals[j][0] < intervals[minIndex][0]) {
-                    minIndex = j;
-                }
-            }
-            int temp = intervals[minIndex][0];
-            intervals[minIndex][0] = intervals[i][0];
-            intervals[i][0] = temp;
-        } */
-        
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
         LinkedList<int[]> list = new LinkedList<>();
         for(int[] interval: intervals) {
             if( list.isEmpty() || list.getLast()[1] < interval[0]) {
