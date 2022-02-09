9b37345478f1b4b7738d22247df8503eb2df3d4c
Inderpreet Singh <31967757+inderSing@users.noreply.github.com>
2022-02-09 12:58:25 -0800

    Time: 8 ms (87.36%), Space: 44.8 MB (28.25%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,61 +1,43 @@
 class Solution {
-    int count;
     int[][] dp;
     public int findTargetSumWays(int[] nums, int target) {
+        int len = nums.length;
         int sum = 0;
-        count = 0;
         for(int i = 0; i < nums.length; i++) {
             sum = sum + nums[i];
         }
-        int t = (target+sum);
-        if( t%2 != 0) {
+        int a = sum + target;
+        if( a%2 != 0) {
             return 0;
         }
-        t = t/2;
-        if( t < 0 ) {
-            int temp = (-1)*t;
-            dp = new int[nums.length+1][temp+1];
-            for(int i = 0; i <= nums.length; i++) {
-                for(int j = 0; j <= temp; j++) {
-                    dp[i][j] = -100;
-                }
-            }
-            return helper(nums, temp, nums.length-1);
+        a = a/2;
+        if( a > 0) {
+            dp = new int[len+1][a+1];
+            return helper(nums, a); 
         } else {
-            dp = new int[nums.length+1][t+1];
-            for(int i = 0; i <= nums.length; i++) {
-                for(int j = 0; j <= t; j++) {
-                    dp[i][j] = -100;
-                }
-            }
-            return helper(nums, t, nums.length-1);
+            int x = a*(-1);
+            dp = new int[len+1][x+1];
+            return helper(nums, x);
         }
-        //dp = new int[nums.length+1][t+1];
-        /*for(int i = 0; i <= nums.length; i++) {
-            for(int j = 0; j <= t; j++) {
-                dp[i][j] = -100;
-            }
-        } */
-        
-        //return dp[nums.length][t];
     }
     
-    public int helper(int[] nums, int target, int n) {
-        if( n == -1 ) {
-            if( target == 0 ) {
-                return 1;
-            } else {
-                return 0;
+    public int helper(int[] nums, int target) {
+        for(int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = 0;
+        }
+        for(int i = 0; i < dp.length; i++) {
+            dp[i][0] = 1;
+        }
+        dp[0][0] = 1;
+        for(int i = 1; i < dp.length; i++) {
+            for(int j = 0; j < dp[0].length; j++) {
+                if( nums[i-1] <= j) {
+                    dp[i][j] = dp[i-1][j - nums[i-1]] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
-        if( dp[n][target] != -100 ) {
-            return dp[n][target];
-        }
-        if( nums[n] <= target ) {
-            dp[n][target] = helper(nums, target - nums[n], n-1) + helper(nums, target, n-1);
-        } else {
-            dp[n][target] = helper(nums, target, n-1);
-        }
-        return dp[n][target];
+        return dp[nums.length][target];
     }
 }
