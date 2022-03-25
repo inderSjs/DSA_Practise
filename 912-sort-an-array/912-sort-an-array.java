641b8b8cc1d438f778cf8478186ed1e1b8a2a3a1
inderSjs <40349728+inderSjs@users.noreply.github.com>
2022-03-25 14:11:43 -0700

    Time: 27 ms (33.78%), Space: 74.4 MB (10.66%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,5 +1,55 @@
 class Solution {
     public int[] sortArray(int[] nums) {
+        sort(nums, 0, nums.length-1);
+        return nums;
+    }
+    
+    public void sort(int[] nums, int l, int r) {
+        if( l < r ) {
+            int mid = l + (r-l)/2;
+            sort(nums, l, mid);
+            sort(nums, mid+1, r);
+            merge(nums, l, mid, r);
+        }
+    }
+    
+    public void merge(int[] arr, int l, int m, int r) {
+        int n1 = m-l+1;
+        int n2 = r-m;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for(int i = 0; i < n1; i++) {
+            L[i] = arr[l+i];
+        }
+        for(int i = 0; i < n2; i++) {
+            R[i] = arr[m+1+i];
+        }
+        int i = 0;
+        int j = 0;
+        int k = l;
+        while( i < n1 && j < n2) {
+            if (L[i] < R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while( i < n1 ) {
+            arr[k] = L[i];
+            k++;
+            i++;
+        }
+        while( j < n2 ) {
+            arr[k] = R[j];
+            k++;
+            j++;
+        }
+    }
+    
+/*    public int[] sortArray(int[] nums) {
         int len = nums.length;
         for(int i = 1; i < len; i++ ) {
             int key = nums[i];
@@ -11,5 +61,5 @@ class Solution {
             nums[j+1] = key;
         }
         return nums;
-    }
+    } */
 }
