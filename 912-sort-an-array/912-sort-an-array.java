3ec085a6d3461c1f9b921dc0cc7533bbc97d25f7
inderSjs <40349728+inderSjs@users.noreply.github.com>
2022-03-30 20:05:37 -0700

    Time: 2337 ms (5.01%), Space: 69.8 MB (47.18%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,45 +1,14 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        int mid = nums.length/2;
-        int i = mid;
-        int j = mid;
-        while ( i >= 0 && j < nums.length ) {
-            swap(nums, i, j);
-            i--;
-            j++;
-        }
-        sort(nums, 0 , nums.length-1);
-        return nums;
-    }
-    
-    public void sort(int[] arr, int start, int end) {
-        if( start < end ) {
-            int pivot = partition(arr, start, end);
-            sort(arr, start, pivot-1);
-            sort(arr, pivot+1, end);
-        }
-    }
-    
-    public int partition(int[] arr, int start, int end) {
-        int pivot = arr[end];
-        int i = start-1;
-        int j = start;
-        while( j < end ) {
-            if( arr[j] < pivot ) {
-                i++;
-                swap(arr, i, j);
-               // i++;
-               // j++;
+        for(int i = 1; i < nums.length; i++) {
+            int j = i-1;
+            int key = nums[i];
+            while( j >= 0 && nums[j] > key) {
+                nums[j+1] = nums[j];
+                j--;
             }
-            j++;
+            nums[j+1] = key;
         }
-        swap(arr, i+1, end);
-        return (i+1);
-    }
-    
-    public void swap(int[] arr, int i ,int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        return nums;
     }
 }
