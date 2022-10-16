6a2720f81818b8122e462893c9a4531da1f83824
inderSjs <40349728+inderSjs@users.noreply.github.com>
2022-10-16 11:19:51 -0700

    Time: 62 ms (5.10%), Space: 75.5 MB (5.05%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,15 +1,9 @@
 class Solution {
-    //public int vertices;
-    public int[][] graph;
+    int[][] graph;
     
     public int countComponents(int n, int[][] edges) {
-        int numberOfConnectedComponents = 0;
+        int result = 0;
         graph = new int[n][n];
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                graph[i][j] = 0;
-            }
-        }
         for(int i = 0; i < edges.length; i++) {
             int fv = edges[i][0];
             int sv = edges[i][1];
@@ -20,16 +14,16 @@ class Solution {
         for(int i = 0; i < n; i++) {
             if( !isVisited[i] ) {
                 dfs(i, isVisited);
-                numberOfConnectedComponents++;
+                result++;
             }
         }
-        return numberOfConnectedComponents;
+        return result;
     }
     
     public void dfs(int vertex, boolean[] isVisited) {
         isVisited[vertex] = true;
-        for(int i = 0; i < graph.length; i++) {
-            if( graph[vertex][i] == 1 && !isVisited[i]) {
+        for(int i = 0; i < isVisited.length; i++) {
+            if( graph[vertex][i] == 1 && !isVisited[i] ) {
                 dfs(i, isVisited);
             }
         }
