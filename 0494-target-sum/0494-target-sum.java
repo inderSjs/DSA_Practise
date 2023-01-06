b4af112e4310d1b1ff2d9cb1db2a55c075182296
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-01-06 11:23:42 +0530

    Time: 10 ms (78.16%), Space: 44 MB (27.15%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,9 +1,8 @@
 class Solution {
     
-    public int count;
+    public int[][] memo;
     
     public int findTargetSumWays(int[] nums, int target) {
-        count = 0;
         int temp = 0;
         for(int i = 0; i < nums.length; i++ ) {
             temp = temp + nums[i];
@@ -13,24 +12,38 @@ class Solution {
             return 0;
         }
         temp = temp/2;
+        if( temp < 0 ) {
+            return 0;
+        }
         //System.out.println(temp);
+        memo = new int[nums.length][temp+1];
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j <= temp; j++) {
+                memo[i][j] = -1;
+            }
+        }
         return helper(nums, nums.length-1, temp);
         //return count;
     }
     
     public int helper(int[] nums, int index, int sum) {
         if( sum == 0 && index < 0) {
-            count = count + 1;
+            //count = count + 1;
             return 1;
         }
         if( index < 0 ) {
             return 0;
         }
+        if( memo[index][sum] != -1 ) {
+            return memo[index][sum];
+        }
         if( nums[index] <= sum ) {
-            return helper(nums, index-1, sum - nums[index]) + helper(nums, index-1, sum);
+            memo[index][sum] = helper(nums, index-1, sum - nums[index]) + helper(nums, index-1, sum);
+            return memo[index][sum];
            
         } else {
-           return helper(nums, index-1, sum);
+            memo[index][sum] = helper(nums, index-1, sum);
+            return memo[index][sum];
         }
     }
 }
