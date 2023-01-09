e19d0f8e7d36137db5cfe2a5a69eb4108e226825
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-01-09 13:06:07 +0530

    Time: 5 ms (79.03%), Space: 49.4 MB (36.35%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,28 +1,29 @@
 class Solution {
     int[][] dp;
     public int change(int amount, int[] coins) {
-        dp = new int[amount+1][coins.length+1];
-        for(int i = 0; i <= amount; i++ ) {
-            for(int j = 0; j <= coins.length; j++) {
+        dp = new int[coins.length][amount+1];
+        for(int i = 0; i < coins.length; i++) {
+            for(int j = 0; j <= amount; j++) {
                 dp[i][j] = -1;
             }
-        } 
-        return helper(coins, amount, coins.length-1);
+        }
+        return helper(coins, coins.length-1, amount);
     }
     
-    public int helper(int[] coins, int amount, int index) {
+    public int helper(int[] coins, int index, int amount) {
         if( amount == 0 ) {
             return 1;
         }
-        if( index < 0 || amount < 0 ) {
+        if( index < 0 ) {
             return 0;
         }
-        if( dp[amount][index] != -1 ) {
-            return dp[amount][index];
+        if( dp[index][amount] != -1 ) {
+            return dp[index][amount];
         }
-        int a = helper(coins, amount - coins[index], index);
-        int b = helper(coins, amount, index -1);
-        dp[amount][index] = (a+b);
-        return dp[amount][index];
+        if( coins[index] <= amount ) {
+            return dp[index][amount] = helper(coins, index, amount - coins[index]) + helper(coins, index-1, amount);
+        } else {
+            return dp[index][amount] = helper(coins, index-1, amount);
+        }
     }
 }
