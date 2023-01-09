a04ddc1aeb7fa2484756fd4b55ee20fb1090c7a9
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-01-09 13:16:22 +0530

    Time: 8 ms (59.74%), Space: 48.9 MB (48.06%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,29 +1,26 @@
 class Solution {
-    int[][] dp;
     public int change(int amount, int[] coins) {
-        dp = new int[coins.length][amount+1];
-        for(int i = 0; i < coins.length; i++) {
-            for(int j = 0; j <= amount; j++) {
-                dp[i][j] = -1;
+        int[][] dp = new int[coins.length+1][amount+1];
+        for( int i = 0; i <= coins.length; i++) {
+            for( int j = 0 ; j <= amount; j++) {
+                if( j == 0 ) {
+                    dp[i][j] = 1;
+                }
+                if( i == 0 ) {
+                    dp[i][j] = 0;
+                }
             }
         }
-        return helper(coins, coins.length-1, amount);
-    }
-    
-    public int helper(int[] coins, int index, int amount) {
-        if( amount == 0 ) {
-            return 1;
+        dp[0][0] = 1;
+        for(int i = 1; i <= coins.length; i++) {
+            for(int j = 1; j <= amount; j++) {
+                if( coins[i-1] <= j) {
+                    dp[i][j] = dp[i][j - coins[i-1]] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
         }
-        if( index < 0 ) {
-            return 0;
-        }
-        if( dp[index][amount] != -1 ) {
-            return dp[index][amount];
-        }
-        if( coins[index] <= amount ) {
-            return dp[index][amount] = helper(coins, index, amount - coins[index]) + helper(coins, index-1, amount);
-        } else {
-            return dp[index][amount] = helper(coins, index-1, amount);
-        }
+        return dp[coins.length][amount];
     }
 }
