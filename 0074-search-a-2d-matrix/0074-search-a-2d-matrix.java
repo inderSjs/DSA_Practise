9148d88976ddacbe2161afedfd9dbf166b163f5e
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-02-28 18:20:43 -0800

    Time: 0 ms (100.00%), Space: 42.1 MB (45.09%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        boolean check = false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+       // if( m == 1 && n == )
+        if( target < matrix[0][0] || target > matrix[m-1][n-1]) {
+            return false;
+        }
+        int i = 0;
+        while( i < m ) {
+            if( target >= matrix[i][0] && target <= matrix[i][n-1]) {
+                check = true;
+                return binarySearch(matrix[i], 0, n-1, target);
+            }
+            i++;
+        }
+        if( !check ) {
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean binarySearch(int[] arr, int l, int r, int target) {
+        if( l == r ) {
+            if( arr[l] == target ) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if( l < r ) {
+            int mid = (l + (r-1))/2;
+            if( arr[mid] == target ) {
+                return true;
+            } else if ( target < arr[mid] ) {
+                return binarySearch(arr, l, mid-1, target);
+            } else {
+                return binarySearch(arr, mid+1, r, target);
+            }
+        }
+        return false;
+    }
+}
