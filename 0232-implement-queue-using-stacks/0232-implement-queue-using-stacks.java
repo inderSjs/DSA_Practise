e9345fd02fcda6c4474c35c3112365b0309f8db1
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-02-28 16:06:41 -0800

    Time: 0 ms (100.00%), Space: 40.4 MB (30.50%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,42 +1,39 @@
 class MyQueue {
     private Stack<Integer> mainStack;
     private Stack<Integer> auxStack;
-    private int counter;
+    private int front;
 
     public MyQueue() {
         mainStack = new Stack<Integer>();
         auxStack = new Stack<Integer>();
-        counter = 0;
     }
     
     public void push(int x) {
-        if( counter == 0 ) {
-            mainStack.push(x);
-            counter++;
-            return;
-        }
-        while( !mainStack.isEmpty() ) {
-            int temp = mainStack.pop();
-            auxStack.push(temp);
+        if( mainStack.isEmpty() ) {
+            front = x;
         }
         mainStack.push(x);
-        while( !auxStack.isEmpty() ) {
-            int temp1 = auxStack.pop();
-            mainStack.push(temp1);
-        }
-        counter++;
     }
     
     public int pop() {
-        return mainStack.pop();
+        if( auxStack.isEmpty() ) {
+            while( !mainStack.isEmpty() ) {
+                auxStack.push(mainStack.pop());
+            }
+        }
+        return auxStack.pop();
     }
     
     public int peek() {
-        return mainStack.peek();
+        if( auxStack.isEmpty() ) {
+            return front;
+        } else {
+            return auxStack.peek();
+        }
     }
     
     public boolean empty() {
-        return mainStack.isEmpty();
+        return mainStack.isEmpty() && auxStack.isEmpty();
     }
 }
 
