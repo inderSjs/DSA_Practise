56dec5e826595d056bfefddcdef89fa2a95dddb4
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-03-05 13:35:06 -0800

    Time: 9 ms (62.21%), Space: 42.3 MB (51.23%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,29 +1,22 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        
-        int[] mappingDictStoT = new int[256];
-        Arrays.fill(mappingDictStoT, -1);
-        
-        int[] mappingDictTtoS = new int[256];
-        Arrays.fill(mappingDictTtoS, -1);
-        
-        for (int i = 0; i < s.length(); ++i) {
+        HashMap<Character, Character> map = new HashMap<>();
+        int n = s.length();
+        for(int i = 0; i < n; i++) {
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
-            
-            // Case 1: No mapping exists in either of the dictionaries
-            if (mappingDictStoT[c1] == -1 && mappingDictTtoS[c2] == -1) {
-                mappingDictStoT[c1] = c2;
-                mappingDictTtoS[c2] = c1;
-            }
-            
-            // Case 2: Ether mapping doesn't exist in one of the dictionaries or Mapping exists and
-            // it doesn't match in either of the dictionaries or both 
-            else if (!(mappingDictStoT[c1] == c2 && mappingDictTtoS[c2] == c1)) {
-                return false;
+            if( map.containsKey(c1) ) {
+                char c3 = map.get(c1);
+                if( c2 != c3 ) {
+                    return false;
+                }
+            } else {
+                if( map.containsValue(c2) ) {
+                    return false;
+                }
+                map.put(c1, c2);
             }
         }
-        
         return true;
     }
 }
