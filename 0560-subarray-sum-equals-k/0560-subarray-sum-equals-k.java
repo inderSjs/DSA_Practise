1e19d902e444132824695962cfaee569d854311c
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-03-06 19:24:30 -0800

    Time: 25 ms (66.07%), Space: 47.6 MB (12.38%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,3 +1,12 @@
+// Time Complexity : O(n) where n is the size of arr
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Yes, the intuition for the time
+// optimized solution was not clear
+
+
+// Your code here along with comments explaining your approach
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
         //map to store prefix sum
