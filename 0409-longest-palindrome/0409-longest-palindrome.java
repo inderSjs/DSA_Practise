c6cc86727192422de658b13240ebdfb6874f64b7
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-03-07 15:07:17 -0800

    Time: 5 ms (56.16%), Space: 42.7 MB (6.60%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,3 +1,14 @@
+// Time Complexity : O(n) where n is the length of the string
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+// Add elements to the HashSet from left to right
+// If the same character is encountered, remove that character
+// from the set and increment count by 2. else just add the character to the set
+
 class Solution {
     public int longestPalindrome(String s) {
         HashSet<Character> set = new HashSet<>();
