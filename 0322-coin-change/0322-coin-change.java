71354c5d0e1c6008d19955b1396a558f60939ff9
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-03-08 21:07:15 -0800

    Time: 24 ms (41.53%), Space: 42.9 MB (23.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,36 +1,31 @@
 class Solution {
-    int[][] memo;
     public int coinChange(int[] coins, int amount) {
-        memo = new int[coins.length][amount+1];
-        for(int i = 0; i < coins.length; i++) {
+        int[][] dp = new int[coins.length+1][amount+1];
+        int a = Integer.MAX_VALUE-1;
+        for(int i = 0; i <= coins.length; i++) {
             for(int j = 0; j <= amount; j++) {
-                memo[i][j] = -1;
+                if( j == 0) {
+                    dp[i][j] = 0;
+                }
+                if( i == 0 ) {
+                    dp[i][j] = a;
+                }
             }
         }
-        int x = helper(coins, coins.length-1, amount);
-        if( x == Integer.MAX_VALUE-1) {
+        dp[0][0] = 0;
+        for(int i = 1; i <= coins.length; i++) {
+            for(int j = 1; j <= amount; j++) {
+                if( j >= coins[i-1]) {
+                    dp[i][j] = Math.min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        if( dp[coins.length][amount] >= a) {
             return -1;
-        }
-        return x;
-    }
-    
-    public int helper(int[] coins, int index, int amount) {
-        if( amount == 0 ) {
-            return 0;
-        }
-        
-        if( index < 0 || amount < 0) {
-            return Integer.MAX_VALUE-1;
-        }
-        if( memo[index][amount] != -1 ) {
-            return memo[index][amount];
-        }
-        if( coins[index] <= amount) {
-            memo[index][amount] = Math.min( 1 + helper(coins, index, amount-coins[index]), helper(coins, index-1, amount));
-            return memo[index][amount];
         } else {
-            memo[index][amount] = helper(coins, index-1, amount);
-            return memo[index][amount];
+            return dp[coins.length][amount];
         }
     }
 }
