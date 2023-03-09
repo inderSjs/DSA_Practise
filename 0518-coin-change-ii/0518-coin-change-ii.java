29d8cf188f1db8fb48477242ec520328814ffa8a
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-03-09 08:34:50 -0800

    Time: 4 ms (69.52%), Space: 39.7 MB (86.09%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,22 +1,18 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int m = coins.length;
-        int[][] dp = new int[m+1][amount+1];
+        int[] dp = new int[amount+1];
         for(int i = 0; i <= amount; i++) {
-            dp[0][i] = 0;
+            dp[i] = 0;
         }
-        for(int i = 0; i <= m; i++) {
-            dp[i][0] = 1;
-        }
+        dp[0] = 1;
         for(int i = 1; i <= m; i++) {
             for(int j = 1; j <= amount; j++) {
                 if( j >= coins[i-1] ) {
-                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
-                } else {
-                    dp[i][j] = dp[i-1][j];
+                    dp[j] = dp[j-coins[i-1]] + dp[j];
                 }
             }
         }
-        return dp[m][amount];
+        return dp[amount];
     }
 }
