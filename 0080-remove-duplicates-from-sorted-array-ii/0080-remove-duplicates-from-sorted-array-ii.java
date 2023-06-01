37b877d82531ae52a0bedb0f553a3650961acc97
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-06-01 08:58:25 -0700

    Time: 0 ms (100.00%), Space: 43.8 MB (33.20%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        return helper(nums, 2);
+    }
+    
+    private int helper(int[] nums, int k) {
+        int n = nums.length;
+        if( n == 1 ) {
+            return 1;
+        }
+        int slow = 1;
+        int fast = 1;
+        int count = 1;
+        while( fast < n ) {
+            if( nums[fast] == nums[fast-1] ) {
+                count++;
+            } else {
+                count = 1;
+            }
+            if( count <= k ) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
