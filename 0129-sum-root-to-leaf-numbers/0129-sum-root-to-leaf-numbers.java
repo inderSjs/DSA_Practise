925b46c29d7328f7824b878dbea1fef217a3138d
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-05-31 18:11:10 -0700

    Time: 1 ms (30.71%), Space: 40.8 MB (11.72%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,24 +14,25 @@
  * }
  */
 class Solution {
-    int sum;
     public int sumNumbers(TreeNode root) {
-        helper(root, 0);
-        return sum;
-    }
-    
-    public void helper(TreeNode root, int currSum) {
-        //base case
-        currSum = currSum*10 + root.val;
-        if( root.left == null && root.right == null ) {
-            sum = sum + currSum;
-            return;
+        Stack<TreeNode> st = new Stack<>();
+        Stack<Integer> mst = new Stack<>();
+        int curr = 0;
+        int result = 0;
+        while(root != null || !st.isEmpty() ) {
+            while( root != null ) {
+                curr = curr*10 + root.val;
+                st.push(root);
+                mst.push(curr);
+                root = root.left;
+            }
+            root = st.pop();
+            curr = mst.pop();
+            if( root.left == null && root.right == null) {
+                result = result + curr;
+            }
+            root = root.right;
         }
-        if(root.left != null ) {
-            helper(root.left, currSum);
-        }
-        if( root.right != null ) {
-            helper(root.right, currSum);
-        }
+        return result;
     }
 }
