980ca3948f440e0d502bf8d2418aec263048a080
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-06-01 14:32:38 -0700

    Time: 1 ms (100.00%), Space: 44.7 MB (7.57%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int mask1 = 0;
+        for(int num: nums) {
+            mask1 = mask1 ^ num;
+        }
+        int temp = mask1 & (-mask1);
+        int x = 0;
+        for(int i: nums) {
+            if( (i & temp) != 0) {
+                x = x ^ i;
+            }
+        }
+        int y = x ^ mask1;
+        return new int[]{x,y};
+    }
+}
