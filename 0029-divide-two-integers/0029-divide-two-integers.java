3542f79dc21a6ef383f5d7c1ca940d6c1e91c2c4
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-06-01 15:19:01 -0700

    Time: 1 ms (100.00%), Space: 39.6 MB (90.97%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if( divisor == 0 ) {
+            return Integer.MAX_VALUE;
+        }
+        if( dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        if( divisor == 1) {
+            return dividend;
+        }
+        int result = 0;
+        long ldividend = Math.abs((long)dividend);
+        long ldivisor = Math.abs((long)divisor);
+        // int shifts = 0;
+        while( ldividend >= ldivisor) {
+            int shifts = 0;
+            while( ldividend >= (ldivisor<<shifts) ) {
+                shifts++;
+            }
+            shifts--;
+            ldividend = ldividend - (ldivisor<<shifts);
+            result = result + (int)Math.pow(2,shifts);
+        }
+        if( (divisor > 0 && dividend >= 0) || (divisor < 0 && dividend < 0) ) {
+            return result;
+        }
+        return -1*result;
+    }
+}
