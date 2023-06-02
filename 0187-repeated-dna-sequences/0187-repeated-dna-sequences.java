33f62ca07dcdc7509f18cdb05cb7feeee967db1b
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-06-02 09:10:45 -0700

    Time: 28 ms (19.44%), Space: 49.6 MB (88.37%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,16 +1,33 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        HashSet<String> set = new HashSet<>();
-        HashSet<String> list = new HashSet<>();
         int n = s.length();
-        // System.out.println(n);
-        for(int i = 0; i <= n-10; i++) {
-            String substr = s.substring(i, i+10);
-            if( set.contains(substr)) {
-                list.add(substr);
-            }
-            set.add(substr);
+        if( n < 10 ) {
+            return new ArrayList<>();
         }
-        return new ArrayList<>(list);
+        HashSet<String> result = new HashSet<>();
+        HashSet<Integer> seen = new HashSet<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('A', 1);
+        map.put('C', 2);
+        map.put('G', 3);
+        map.put('T', 4);
+        int hash = 0;
+        int temp = (int)Math.pow(4, 10);
+        for(int i = 0; i < n; i++) {
+            char in = s.charAt(i);
+            hash = hash*4 + map.get(in);
+            if( i > 9 ) {
+                char out = s.charAt(i-10);
+                hash = hash - map.get(out)*temp;
+            }
+            if( i > 8 ) {
+                if( seen.contains(hash) ) {
+                    result.add(s.substring(i-9, i+1));
+                }
+                seen.add(hash);
+            }
+            
+        }
+        return new ArrayList<>(result);
     }
 }
