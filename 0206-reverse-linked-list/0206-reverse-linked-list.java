ed0b261e393732e0838d6da6b90c89374d833bf8
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-06-05 09:00:35 -0700

    Time: 0 ms (100.00%), Space: 41.5 MB (92.32%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -13,21 +13,13 @@ class Solution {
         if( head == null || head.next == null ) {
             return head;
         }
-        ListNode newHead = head.next;
-        ListNode temp = newHead.next;
-        head.next = null;
-        while( temp != null ) {
-            newHead.next = head;
-            head = newHead;
-            newHead = temp;
-            temp = temp.next;
+        ListNode prev = null;
+        while( head != null ) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
         }
-        newHead.next = head;
-        // ListNode tt = newHead;
-        // while(tt != null ) {
-        //     System.out.print(tt.val + "->");
-        //     tt = tt.next;
-        // }
-        return newHead;
+        return prev;
     }
 }
