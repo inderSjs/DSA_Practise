099a19b0df36281f41e6bcc2111026751f846e57
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-06-06 15:37:54 -0700

    Time: 2 ms (96.35%), Space: 43.6 MB (11.95%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,26 +14,29 @@
  * }
  */
 class Solution {
+    HashMap<Integer, Integer> map;
+    int postIdx;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        int n = postorder.length;
-        if( n == 0) {
+        int n = postorder.length-1;
+        this.postIdx = n;
+        this.map = new HashMap<>();
+        for(int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return helper(postorder, 0 , n);
+    }
+    
+    private TreeNode helper(int[] postorder, int start, int end) {
+        if( start > end) {
             return null;
         }
-        int rootVal = postorder[n-1];
+        int rootVal = postorder[postIdx];
+        int rootIdx = map.get(rootVal);
+        postIdx--;
         TreeNode root = new TreeNode(rootVal);
-        int rootidx = -1;
-        for(int i = 0; i < n; i++) {
-            if( inorder[i] == rootVal) {
-                rootidx = i;
-                break;
-            }
-        }
-        int[] inleft = Arrays.copyOfRange(inorder, 0, rootidx);
-        int[] inright = Arrays.copyOfRange(inorder, rootidx+1, n);
-        int[] postleft = Arrays.copyOfRange(postorder, 0, inleft.length);
-        int[] postright = Arrays.copyOfRange(postorder, postleft.length, n-1 );
-        root.right = buildTree(inright, postright);
-        root.left = buildTree(inleft, postleft);
+        root.right = helper(postorder, rootIdx+1, end);
+        root.left = helper(postorder, start, rootIdx-1);
+        
         return root;
     }
 }
