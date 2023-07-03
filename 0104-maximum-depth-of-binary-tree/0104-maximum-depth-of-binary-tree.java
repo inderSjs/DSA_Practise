32b10dc33efe28f1b72041f0501888613d0bf6e4
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-07-03 13:58:13 -0700

    Time: 0 ms (100.00%), Space: 41.8 MB (57.46%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,26 +14,16 @@
  * }
  */
 class Solution {
-    int max;
     public int maxDepth(TreeNode root) {
+        return helper(root);
+    }
+    
+    public int helper(TreeNode root) {
         if( root == null ) {
             return 0;
         }
-        max = 0;
-        helper(root, 1);
-        return max;
-    }
-    
-    private void helper(TreeNode root, int h) {
-        if( root.left == null && root.right == null ) {
-            max = Math.max(max, h);
-            return;
-        }
-        if( root.left != null ) {
-            helper(root.left, h+1);
-        }
-        if( root.right != null ) {
-            helper(root.right, h+1);
-        }
+        int lh = helper(root.left);
+        int rh = helper(root.right);
+        return 1 + Math.max(lh, rh);
     }
 }
