d06313da0763befe1693df863ecdb3bfa1eac91c
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-07-13 13:03:56 -0700

    Time: 99 ms (81.37%), Space: 44.5 MB (65.69%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.java
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -2,31 +2,43 @@ class Solution {
     List<String> result;
     public List<String> addOperators(String num, int target) {
         result = new ArrayList<>();
-        helper(num, target, 0, 0l, 0l, "");
+        helper(num, target, 0, 0l, 0l, new StringBuilder());
         return result;
     }
     
-    private void helper(String num, int target, int pivot, long calc, long tail, String expression) {
+    private void helper(String num, int target, int pivot, long calc, long tail, StringBuilder expression) {
         //base case
         if( pivot == num.length() ) {
             if( calc == target ) {
-                result.add(expression);
+                result.add(expression.toString());
             }
             return;
         }
         
         //logic
         for(int i = pivot; i < num.length(); i++) {
+            int le = expression.length();
             long curr = Long.parseLong(num.substring(pivot, i+1));
             if( i != pivot && num.charAt(pivot) == '0') {
                 continue;
             }
             if( pivot == 0 ) {
-                helper(num, target, i+1, curr, curr, expression+curr);
+                expression.append(curr);
+                helper(num, target, i+1, curr, curr, expression);
+                expression.setLength(le);
             } else {
-                helper(num, target, i+1, calc+curr, curr, expression + "+" + curr);
-                helper(num, target, i+1, calc-curr, -curr, expression +"-" +curr);
-                helper(num, target, i+1, calc-tail + (curr*tail), curr*tail, expression +"*" +curr);
+                expression.append("+");
+                expression.append(curr);
+                helper(num, target, i+1, calc+curr, curr, expression);
+                expression.setLength(le);
+                expression.append("-");
+                expression.append(curr);
+                helper(num, target, i+1, calc-curr, -curr, expression);
+                expression.setLength(le);
+                expression.append("*");
+                expression.append(curr);
+                helper(num, target, i+1, calc-tail + (curr*tail), curr*tail, expression);
+                expression.setLength(le);
             }
         }
     }
