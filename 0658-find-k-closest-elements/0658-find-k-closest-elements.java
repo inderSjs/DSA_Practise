fa8e0f6bc9c6167c675d061d0cef6cbbc2bdcdc1
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-07-21 10:00:14 -0700

    Time: 5 ms (69.06%), Space: 45.2 MB (43.14%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,29 +1,23 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < arr.length; i++) {
-            int dist = Math.abs(x-arr[i]);
-            map.put(i, dist);
-        }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> {
-            int distA = map.get(a);
-            int distB = map.get(b);
-            if( distA == distB ) {
-                return b-a;
-            }
-            return distB-distA;
-        });
-        for(int i = 0; i < arr.length; i++) {
-            pq.add(i);
-            if( pq.size() > k ) {
-                pq.poll();
-            }
-        }
+        //2 pointer solution
+        int start = 0;
+        int end = arr.length-1;
         List<Integer> result = new ArrayList<>();
-        while( !pq.isEmpty() ) {
-            result.add(arr[pq.poll()]);
+        while( (end-start+1) > k ) {
+            int distA = Math.abs(x - arr[start]);
+            int distB = Math.abs(x - arr[end]);
+            if( distA == distB ) {
+                end--;
+            } else if ( distA > distB ) {
+                start++;
+            } else {
+                end--;
+            }
         }
-        Collections.sort(result);
+        for(int i = start; i <= end; i++) {
+            result.add(arr[i]);
+        }
         return result;
     }
 }
