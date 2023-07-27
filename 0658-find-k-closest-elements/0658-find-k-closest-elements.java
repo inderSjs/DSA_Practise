b663310d417d4324db278260e4f1c3eca1ff300a
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-07-27 13:32:25 -0700

    Time: 3 ms (99.77%), Space: 45.2 MB (56.48%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,21 +1,21 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> result = new ArrayList<>();
         int low = 0;
         int high = arr.length - k;
         while( low < high ) {
-            int mid = low + (high - low)/2;
+            int mid = low + (high-low)/2;
             int distS = x - arr[mid];
             int distE = arr[mid+k] - x;
             if( distS > distE ) {
-                low = mid + 1;
+                low = mid+1;
             } else {
                 high = mid;
             }
         }
-        for(int i = low; i < low + k; i++) {
-            result.add(arr[i]);
+        List<Integer> list = new ArrayList<>();
+        for(int i = low; i < low+k; i++) {
+            list.add(arr[i]);
         }
-        return result;
+        return list;
     }
 }
