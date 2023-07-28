e4cf788e16dec6c0a7bd687112a664d49d29cf2d
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-07-28 13:04:58 -0700

    Time: 3 ms (83.31%), Space: 43.4 MB (99.08%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,92 @@
+class Solution {
+    List<List<String>> result;
+    public List<List<String>> solveNQueens(int n) {
+        result = new ArrayList<>();
+        boolean[][] grid = new boolean[n][n];
+        backtrack(grid, 0, n);
+        return result;
+    }
+    
+    private void backtrack(boolean[][] grid, int i, int n) {
+        //base case
+        if( i == n) {
+            List<String> list = new ArrayList<>();
+            for(int x = 0; x < n; x++) {
+                StringBuilder sb = new StringBuilder();
+                for(int y = 0; y < n; y++) {
+                    if( grid[x][y] ) {
+                        sb.append("Q");
+                    } else {
+                        sb.append(".");
+                    }
+                }
+                list.add(sb.toString());
+            }
+            result.add(list);
+            return;
+        }
+        
+        //logic
+        for(int j = 0; j < n; j++) {
+            if( isSafe(grid, i, j) ) {
+                grid[i][j] = true;
+                backtrack(grid, i+1, n);
+                grid[i][j] = false;
+            }
+        }
+    }
+    
+    private boolean isSafe(boolean[][] grid, int i, int j) {
+        int n = grid.length;
+        //row check
+        for(int r = 0; r < n; r++) {
+            if( r != i && grid[r][j] ) {
+                return false;
+            }
+        }
+        //column check
+        for(int c = 0; c < n; c++) {
+            if( c != j && grid[i][c] ) {
+                return false;
+            }
+        }
+        //diagonal check
+        int x = i;
+        int y = j;
+        while( x >= 0 && y >= 0) {
+            x = x-1;
+            y = y-1;
+            if( x >= 0 && y >= 0 && grid[x][y] ) {
+                return false;
+            }
+        }
+        x = i;
+        y = j;
+        while( x >= n && y >= n) {
+            x = x+1;
+            y = y+1;
+            if( x >= n && y >= n && grid[x][y] ) {
+                return false;
+            }
+        }
+        x = i;
+        y = j;
+        while( x >= 0 && y < n) {
+            x = x-1;
+            y = y+1;
+            if(x >= 0 && y < n && grid[x][y] ) {
+                return false;
+            }
+        }
+        x = i;
+        y = j;
+        while( x < n && y >= 0 ) {
+            x = x+1;
+            y = y-1;
+            if( x < n && y >= 0 && grid[x][y] ) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
