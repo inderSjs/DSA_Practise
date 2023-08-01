fd4c0fcc52b22c4d2fce3deca4b47f49f3744f0b
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-08-01 16:14:08 -0700

    Time: 7 ms (60.08%), Space: 45.7 MB (46.64%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,28 +1,19 @@
 class Solution {
-    // private int result;
-    private int memo[][];
     public int change(int amount, int[] coins) {
-        this.memo = new int[coins.length][amount+1];
-        for(int i = 0; i < coins.length; i++) {
+        int[][] dp = new int[coins.length+1][amount+1];
+        for(int i = 0; i <= amount; i++) {
+            dp[0][i] = 0;
+        }
+        dp[0][0] = 1;
+        for(int i = 1; i <= coins.length; i++) {
             for(int j = 0; j <= amount; j++) {
-                memo[i][j] = -1;
+                if( j < coins[i-1] ) {
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j - coins[i-1]];
+                }
             }
         }
-        return helper(coins, amount, 0);
-        // return result;
-    }
-    
-    private int helper(int[] coins, int amount, int index) {
-        if( amount < 0 || index >= coins.length) {
-            return 0;
-        }
-        if( amount == 0) {
-            return 1;
-        }
-        if( memo[index][amount] != -1) {
-            return memo[index][amount];
-        }
-        memo[index][amount] = helper(coins, amount, index+1) + helper(coins, amount-coins[index], index);
-        return memo[index][amount];
+        return dp[coins.length][amount];
     }
 }
