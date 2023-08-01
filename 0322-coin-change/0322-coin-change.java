85017559b2cf74d39f11600f05e7fb9c1a21b008
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-07-31 17:06:12 -0700

    Time: 15 ms (89.59%), Space: 43.6 MB (39.51%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,37 +1,26 @@
 class Solution {
-    int[][] memo;
     public int coinChange(int[] coins, int amount) {
-        this.memo = new int[coins.length][amount+1];
-        for(int i = 0; i < coins.length; i++) {
-            for(int j = 0; j <= amount; j++) {
-                memo[i][j] = -1;
+        int m = coins.length;
+        int n = amount;
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 1; i <= n; i++) {
+            dp[0][i] = amount+10;
+        }
+        for(int i = 0; i <= m; i++) {
+            dp[i][0] = 0;
+        }
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <=n; j++) {
+                if( coins[i-1] > j ) {
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = Math.min( dp[i-1][j], 1 + dp[i][j-coins[i-1]]);
+                }
             }
         }
-        int result =  helper(coins, amount, coins.length-1);
-        if( result == Integer.MAX_VALUE-1) {
+        if( dp[m][n] >= amount+10) {
             return -1;
         }
-        return result;
-    }
-    
-    private int helper(int[] coins, int amount, int idx) {
-        //base case
-        if( amount == 0 ) {
-            return 0;
-        }
-        
-        if( amount < 0 || idx < 0) {
-            return Integer.MAX_VALUE-1;
-        }
-        if( memo[idx][amount] != -1) {
-            return memo[idx][amount];
-        }
-        if( amount >= coins[idx] ) {
-            memo[idx][amount] = Math.min( 1 + helper(coins, amount-coins[idx], idx), helper(coins, amount, idx-1));
-            return memo[idx][amount];
-        } else {
-            memo[idx][amount] = helper(coins, amount, idx-1);
-            return memo[idx][amount];
-        }
+        return dp[m][n];
     }
 }
