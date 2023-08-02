c44333d60136680f39ecb529daee3121e99175fb
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-08-02 13:39:01 -0700

    Time: 0 ms (100.00%), Space: 44.6 MB (47.35%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,56 +1,54 @@
 class Solution {
-    
-    public int findFirst(int[] arr, int target) {
-        int low = 0;
-        int high = arr.length-1;
-        while( low <= high ) {
-            int mid = low + (high-low)/2;
-            if( arr[mid] == target) {
-                if( mid == 0 || arr[mid-1] != target) {
-                    return mid;
-                }
-                if( arr[mid-1] == target ) {
-                    high = mid - 1;
-                }
-            } else if( arr[mid] < target ) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return -1;
-    }
-    
-    public int findLast(int[] arr, int target) {
-        int low = 0;
-        int high = arr.length-1;
-        int n = high;
-        while( low <= high ) {
-            int mid = low + (high-low)/2;
-            if( arr[mid] == target) {
-                if( mid == n || arr[mid+1] != target) {
-                    return mid;
-                }
-                if( arr[mid+1] == target ) {
-                    low = mid + 1;
-                }
-            } else if( arr[mid] < target ) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return -1;
-    }
-    
     public int[] searchRange(int[] nums, int target) {
         int[] result = new int[]{-1,-1};
-        if( nums.length == 0 ) {
+        if( nums.length == 0) {
             return result;
         }
-        int x = findFirst(nums, target);
-        int y = findLast(nums, target);
-        return new int[]{x,y};
+        int low = 0;
+        int high = nums.length-1;
+        int mid = 0;
+        while( low <= high ) {
+            mid = low + (high-low)/2;
+            if( nums[mid] == target) {
+                if( mid == 0) {
+                    result[0] = mid;
+                    break;
+                }
+                if( nums[mid-1] < target ) {
+                    result[0] = mid;
+                    break;
+                } else {
+                    high = mid - 1;
+                }
+            } else if( nums[mid] > target) {
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        
+        high = nums.length-1;
+        low = mid;
+        while( low <= high) {
+            mid = low + (high-low)/2;
+            if( nums[mid] == target) {
+                if( mid == nums.length-1) {
+                    result[1] = mid;
+                    break;
+                }
+                if( nums[mid] < nums[mid+1]) {
+                    result[1] = mid;
+                    break;
+                } else {
+                    low = mid + 1;
+                }
+            } else if ( nums[mid] < target ) {
+                low = mid+1;
+            } else {
+                high = mid -1;
+            }
+        }
+        
+        return result;
     }
-    
 }
