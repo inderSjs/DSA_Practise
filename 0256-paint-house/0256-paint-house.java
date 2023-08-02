788e57619769ead584be24e2855814fee12b858c
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-08-02 09:43:59 -0700

    Time: 2 ms (18.48%), Space: 42.8 MB (50.27%) - LeetHub

--- 0256-paint-house/0256-paint-house.java
+++ 0256-paint-house/0256-paint-house.java
@@ -1,33 +1,25 @@
 class Solution {
-    private int[][] memo;
     public int minCost(int[][] costs) {
         int n = costs.length;
-        this.memo = new int[3*n][40*n];
-        for(int i = 0; i < (3*n); i++) {
-            for(int j = 0; j < (40*n); j++) {
-                memo[i][j] = -1;
+        int[][] dp = new int[n][3];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 3; j++) {
+                dp[i][j] = costs[i][j];
             }
         }
-        int a = helper(costs, 0, 1, costs[0][0]);
-        int b = helper(costs, 1, 1, costs[0][1]);
-        int c = helper(costs, 2, 1, costs[0][2]);
-        return Math.min(a , Math.min(b,c));
-    }
-    
-    private int helper(int[][] costs, int i, int idx, int amount) {
-        if( idx == costs.length) {
-            return amount;
+        for(int i = n-2; i >= 0; i--) {
+            for(int j = 0; j < 3; j++) {
+                int temp = Integer.MAX_VALUE;
+                int k = 0;
+                while( k < 3) {
+                    if( j != k) {
+                        temp = Math.min(temp, dp[i+1][k]);
+                    }
+                    k++;
+                }
+                dp[i][j] = dp[i][j] + temp;
+            }
         }
-        int i1 = idx*3 + i;
-        if( memo[i1][amount] != -1 ) {
-            return memo[i1][amount];
-        }
-        int x = (i+1)%3;
-        int y = (i+2)%3;
-        int case1 = helper(costs, x, idx+1, amount + costs[idx][x]);
-        int case2 = helper(costs, y, idx+1, amount + costs[idx][y]);
-        int t4 = Math.min(case1, case2);
-        memo[i1][amount] = t4;
-        return t4;
+        return Math.min(dp[0][0], Math.min(dp[0][1], dp[0][2]));
     }
 }
