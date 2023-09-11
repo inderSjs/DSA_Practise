d1f057f7fb4b5ab2de62b6873e267aa221f25507
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-09-11 14:16:31 -0700

    Time: 1 ms (100.00%), Space: 44.8 MB (25.09%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,36 +1,35 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int[] nums = new int[m+n];
-        int p1 = 0;
-        int p2 = 0;
-        int idx = 0;
-        while( p1 < m && p2 < n) {
-            if( nums1[p1] <= nums2[p2] ) {
-                nums[idx] = nums1[p1];
-                p1++;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if( n1 == 0 && n2 == 0) {
+            return 0.0;
+        }
+        if(n1 > n2) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int low = 0;
+        int high = n1;
+        while( low <= high ) {
+            int partX = low + (high-low)/2;
+            int partY = (n1+n2)/2 - partX;
+            int L1 = partX == 0 ? Integer.MIN_VALUE : nums1[partX-1];
+            int R1 = partX == n1 ? Integer.MAX_VALUE : nums1[partX];
+            int L2 = partY == 0 ? Integer.MIN_VALUE : nums2[partY-1];
+            int R2 = partY == n2 ? Integer.MAX_VALUE : nums2[partY];
+            if( L1 <= R2 && L2 <= R1) {
+                if( (n1+n2)%2 == 0) {
+                    return (Math.max(L1, L2) + Math.min(R1, R2))/2.0;
+                } else {
+                    return Math.min(R1, R2)/1.0;
+                }
+                
+            } else if (L1 > R2) {
+                high = partX-1;
             } else {
-                nums[idx] = nums2[p2];
-                p2++;
+                low = partX + 1;
             }
-            idx++;
         }
-        while( p1 < m) {
-            nums[idx] = nums1[p1];
-            p1++;
-            idx++;
-        }
-        while( p2 < n) {
-            nums[idx] = nums2[p2];
-            p2++;
-            idx++;
-        }
-        int mid = (m+n-1)/2;
-        if( (m+n)%2 == 0) {   
-            return ( nums[mid] + nums[mid+1])/2.0;
-        } else {
-            return nums[mid]/1.0;
-        }
+        return 1.908;
     }
 }
