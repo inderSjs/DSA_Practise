2e564afd4ef9cac1ee210e9d4add72501ac1c2e1
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-09-16 08:11:34 -0700

    Time: 7 ms (25.00%), Space: 43.7 MB (88.24%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,49 +10,26 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> l1 = new ArrayList<>();
-        List<TreeNode> l2 = new ArrayList<>();
         TreeNode temp = root;
-        // l1.add(temp);
-        while( temp != p) {
-            l1.add(temp);
-            if( p.val < temp.val ) {
+        TreeNode p1 = null;
+        TreeNode q1 = null;
+        if( p.val < q.val ) {
+            p1 = p;
+            q1 = q;
+        } else {
+            p1 = q;
+            q1 = p;
+        }
+        while( temp != null) {
+            if( p1.val <= temp.val && q1.val >= temp.val) {
+                return temp;
+            }
+            if( p1.val < temp.val && q1.val < temp.val) {
                 temp = temp.left;
             } else {
                 temp = temp.right;
             }
         }
-        l1.add(temp);
-        temp = root;
-        while( temp != q) {
-            l2.add(temp);
-            if( q.val < temp.val ) {
-                temp = temp.left;
-            } else {
-                temp = temp.right;
-            }
-        }
-        l2.add(temp);
-        if( l1.size() > l2.size() ) {
-            return helper(l1, l2);
-        }
-        return helper(l2, l1);
-    }
-    
-    public TreeNode helper(List<TreeNode> l1, List<TreeNode> l2) {
-        // //l1 is always bigger than l2
-        // for(int i = 0; i < l1.size(); i++) {
-        //     System.out.print(l1.get(i).val + " ");
-        // }
-        // System.out.println();
-        // for(int i = 0; i < l2.size(); i++) {
-        //     System.out.print(l2.get(i).val + " ");
-        // }
-        for(int i = 0; i < l2.size(); i++) {
-            if( l1.get(i).val != l2.get(i).val) {
-                return l1.get(i-1);
-            }
-        }
-        return l2.get(l2.size()-1);
+        return temp;
     }
 }
