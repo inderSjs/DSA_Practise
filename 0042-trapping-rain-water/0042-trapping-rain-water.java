2e7f9e1ac20ab4ecea3ee28f4d178baf0904ecba
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-10-01 14:02:32 -0700

    Time: 1 ms (76.29%), Space: 44 MB (74.32%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,21 +1,19 @@
-// Brute Force Solution
-
 class Solution {
     public int trap(int[] height) {
         int result = 0;
         int n = height.length;
+        int[] left_max = new int[n];
+        int[] right_max = new int[n];
+        left_max[0] = height[0];
+        right_max[n-1] = height[n-1];
+        for(int i = 1; i < n; i++) {
+            left_max[i] = Math.max(height[i], left_max[i-1]);
+        }
+        for(int i = n-2; i >= 0; i--) {
+            right_max[i] = Math.max(height[i], right_max[i+1]);
+        }
         for(int i = 0; i < n; i++) {
-            int left_max = 0;
-            int right_max = 0;
-            //iterate over left
-            for(int j = i; j >= 0; j--) {
-                left_max = Math.max(left_max, height[j]);
-            }
-            //iterate over right
-            for(int j = i; j < n; j++) {
-                right_max = Math.max(right_max, height[j]);
-            }
-            result = result + Math.min(left_max, right_max) - height[i];
+            result = result + Math.min(left_max[i], right_max[i]) - height[i];
         }
         return result;
     }
