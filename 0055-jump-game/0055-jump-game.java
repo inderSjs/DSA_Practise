40a2dcd12cf67a4f7cb132ce950bf2e69910dcda
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-10-17 14:47:25 -0700

    Time: 1 ms (99.99%), Space: 44.1 MB (69.83%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,26 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        if( n == 1) {
-            return true;
-        }
-        Queue<Integer> q = new LinkedList<>();
-        HashSet<Integer> set = new HashSet<>();
-        q.add(0);
-        set.add(0);
-        while( !q.isEmpty() ) {
-            int idx = q.poll();
-            for(int i = 1; i <= nums[idx]; i++) {
-                int index = i + idx;
-                if( index < n && !set.contains(index)) {
-                    q.add(index);
-                    set.add(index);
-                }
-                if( index >= (n-1)) {
-                    return true;
-                }
+        int target = nums.length-1;
+        for(int i = n-2; i >= 0; i--) {
+            if( i + nums[i] >= target) {
+                target = i;
             }
         }
-        return false;
+        return (target == 0);
     }
 }
