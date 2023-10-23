5bddcef42b5a2c894d97e33ca33b1f904269bdeb
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-10-23 11:44:29 -0700

    Time: 1 ms (76.31%), Space: 39.4 MB (58.82%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,26 +1,5 @@
 class Solution {
-    public boolean isPowerOfFour(int n) {
-        int temp = n;
-        if( n == -1 ) {
-            return false;
-        }
-        if( n > 0 ) {
-            return helper(n);
-        } else {
-            return false;
-        }
-    }
-    
-    private boolean helper(int n) {
-        if( n == 1) return true;
-        int temp = (int)Math.sqrt(n);
-        int check = 1;
-        for(int i = 1; i < temp; i++) {
-            check = check*4;
-            if( check == n ) {
-                return true;
-            }
-        }
-        return false;
-    }
+  public boolean isPowerOfFour(int num) {
+    return (num > 0) && (Math.log(num) / Math.log(2) % 2 == 0);
+  }
 }
