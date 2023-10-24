17ab34fb20bd3ac9d1dffa0b8bd79202637f1449
inderSjs <40349728+inderSjs@users.noreply.github.com>
2023-10-24 11:33:38 -0700

    Time: 11 ms (13.05%), Space: 41.7 MB (20.84%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> result = new ArrayList<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        int l = s.length();
+        for(int i = 0; i < l; i++) {
+            char ch = s.charAt(i);
+            if( map.containsKey(ch) ) {
+                map.put( ch, i);
+            }
+            map.put(ch, i);
+        }
+        int i = 0;
+        while( i < l) {
+            char first = s.charAt(i);
+            int currWindow = map.get(first);
+            for(int j = i+1; j < currWindow; j++) {
+                char temp = s.charAt(j);
+                int lastIdx = map.get(temp);
+                currWindow = Math.max(currWindow, lastIdx);
+            }
+            result.add(currWindow-i+1);
+            i = currWindow+1;
+        }
+        return result;
+    }
+}
