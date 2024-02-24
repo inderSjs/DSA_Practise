dcadc38f42028acae0709dcbc7c9551b39c2557f
inderSjs <40349728+inderSjs@users.noreply.github.com>
2024-02-23 16:40:46 -0800

    Time: 2 ms (98.16%), Space: 44.6 MB (77.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,15 +4,13 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         int length = nums.length;
         for(int i = 0; i < length; i++) {
-            map.put(nums[i], i);
-        }
-        for(int i = 0; i < length; i++){
             int complement = target - nums[i];
-            if( map.containsKey(complement) && (map.get(complement) != i) ) {
+            if( map.containsKey(complement) && (map.get(complement) != i)) {
                 result[0] = i;
                 result[1] = map.get(complement);
                 break;
             }
+            map.put(nums[i], i);
         }
         return result;  
     }
