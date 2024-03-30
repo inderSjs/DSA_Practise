9693a6a4e1bd0cf4cb92004bc30ccb8e71a86768
inderSjs <40349728+inderSjs@users.noreply.github.com>
2024-03-30 14:42:04 -0700

    Time: 17 ms (22.27%), Space: 48.3 MB (66.83%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,42 +1,33 @@
-class Tuple {
-    int a;
-    int b;
-    
-    public Tuple(int a, int b) {
-        this.a = a;
-        this.b = b;
-    }
-}
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        //Inefficient approach
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int num: nums) {
-            if( !map.containsKey(num)) {
-                map.put(num,0);
+            if( !map.containsKey(num) ) {
+                map.put(num, 0);
             }
-            int cnt = map.get(num);
-            map.put(num, ++cnt);
+            int count = map.get(num);
+            count++;
+            map.put(num, count);
         }
-        PriorityQueue<Tuple> pq = new PriorityQueue<>( new Comparator<Tuple>() {
-            public int compare(Tuple x, Tuple y) {
-                return x.b - y.b;
+        PriorityQueue<Integer> pq = new PriorityQueue<>( new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                return (int)(map.get(a) - map.get(b));
             }
         });
-        for(Map.Entry<Integer, Integer> el: map.entrySet()) {
-            pq.add(new Tuple(el.getKey(), el.getValue()));
-            if( pq.size() > k ) {
+        for(Map.Entry<Integer, Integer> el: map.entrySet() ) {
+            pq.add(el.getKey());
+            if( pq.size() > k) {
                 pq.poll();
             }
         }
-        List<Integer> arr = new ArrayList<>();
-        while( pq.size() != 0) {
-            arr.add(pq.poll().a);
+        List<Integer> result = new ArrayList<>();
+        while( !pq.isEmpty() ) {
+            result.add(pq.poll());
         }
-        int[] result = new int[arr.size()];
-        for(int i = 0; i < arr.size(); i++) {
-            result[i] = arr.get(i);
+        int[] ans = new int[result.size()];
+        for(int i = 0 ; i < result.size(); i++) {
+            ans[i] = result.get(i);
         }
-        return result;
+        return ans;
     }
 }
