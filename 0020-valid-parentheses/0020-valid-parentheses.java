1a354e03982c0ec7d3d28dcddf60e9f6d63dbb4e
inderSjs <40349728+inderSjs@users.noreply.github.com>
2024-04-08 19:36:42 -0700

    Time: 1 ms (98.58%), Space: 41.1 MB (87.16%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,15 @@
-// Time Complexity: O(n) where n is the length of the string
-// Space Complexity: O(n)
-
 class Solution {
     public boolean isValid(String s) {
-        int l = s.length();
-        if( l%2 != 0 ) {
-            return false;
-        } 
         Stack<Character> st = new Stack<>();
-        for(int i = 0; i < l; i++) {
+        for(int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if( ch == '(') {
+            if( ch == '(' ) {
                 st.push(')');
-            } else if( ch == '[') {
+            } else if( ch == '[' ) {
                 st.push(']');
             } else if( ch == '{') {
                 st.push('}');
-            } else if( st.isEmpty() || ch != st.pop() ) {
+            } else if( st.isEmpty() || ch != st.pop()) {
                 return false;
             }
         }
