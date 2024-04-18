0e784c2239129e19ecc602a24cc968be938610c1
inderSjs <40349728+inderSjs@users.noreply.github.com>
2024-04-17 17:14:07 -0700

    Time: 0 ms (100.00%), Space: 44.5 MB (42.33%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,14 +11,28 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        HashSet<ListNode> set = new HashSet<>();
-        while( head != null) {
-            if( set.contains(head) ) {
-                return head;
-            }
-            set.add(head);
-            head = head.next;
+        if( head == null ) {
+            return head;
         }
-        return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean flag = false;
+        while(fast != null && fast.next != null ) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if( slow == fast ) {
+                flag = true;
+                break;
+            }
+        }
+        if( !flag ) {
+            return null;
+        }
+        slow = head;
+        while( slow != fast ) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
     }
 }
