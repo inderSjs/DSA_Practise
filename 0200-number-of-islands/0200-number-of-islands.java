6b86c7082f86c333b5f0d9bea9103af1f4367f77
inderSjs <40349728+inderSjs@users.noreply.github.com>
2024-04-20 13:33:50 -0700

    Time: 4 ms (47.25%), Space: 49.1 MB (76.43%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,19 +1,19 @@
 class Solution {
-    private boolean[][] isVisited;
-    private int m;
-    private int n;
-    private int[][] dirs;
+    int m;
+    int n;
+    boolean[][] isVisited;
+    int[][] dirs;
     
     public int numIslands(char[][] grid) {
         this.m = grid.length;
         this.n = grid[0].length;
         this.isVisited = new boolean[m][n];
-        this.dirs = new int[][]{{0,1}, {1,0}, {0,-1}, {-1,0}};
+        this.dirs = new int[][]{{0,1},{1,0},{0,-1}, {-1,0}};
         int result = 0;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if( grid[i][j] == '1'  && !isVisited[i][j] ) {
-                    dfs(grid, i ,j);
+                if( grid[i][j] == '1' && !isVisited[i][j] ) {
+                    dfs(grid, i, j);
                     result++;
                 }
             }
@@ -21,13 +21,13 @@ class Solution {
         return result;
     }
     
-    private void dfs(char[][] grid, int i, int j) {
-        isVisited[i][j] = true;
+    private void dfs(char[][] grid, int x, int y) {
+        isVisited[x][y] = true;
         for(int[] dir: dirs) {
-            int nr = i + dir[0];
-            int nc = j + dir[1];
-            if( nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == '1' && !isVisited[nr][nc]) {
-                dfs(grid, nr, nc);
+            int i = x + dir[0];
+            int j = y + dir[1];
+            if( i >= 0 && i < m && j >= 0 && j < n && grid[i][j] == '1' && !isVisited[i][j] ) {
+                dfs(grid, i, j);
             }
         }
     }
