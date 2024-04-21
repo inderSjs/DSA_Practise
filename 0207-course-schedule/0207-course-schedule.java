d7c43ae8a058cdad17919d21952a8591397df688
inderSjs <40349728+inderSjs@users.noreply.github.com>
2024-04-20 20:30:58 -0700

    Time: 7 ms (36.34%), Space: 44.5 MB (64.31%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,53 +1,38 @@
 class Solution {
-    int[][] edges;
-    int n;
-    
+        
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        n = numCourses;
-        edges = new int[n][n];
-        ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<Integer> order = new ArrayList<>();
-        for(int i = 0; i < prerequisites.length; i++) {
-            int fv = prerequisites[i][1];
-            int sv = prerequisites[i][0];
-            edges[fv][sv] = 1;
+        int[] indegree = new int[numCourses];
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        for(int[] edge: prerequisites) {
+            int in = edge[1];
+            int out = edge[0];
+            indegree[in]++;
+            if( !map.containsKey(out) ) {
+                map.put(out, new ArrayList<>());
+            }
+            map.get(out).add(in);
         }
-        for(int i = 0; i < n; i++) {
-            if( helper(i) ) {
-                list.add(i);
+        int count = 0;
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++) {
+            if( indegree[i] == 0) {
+                q.add(i);
+                count++;
             }
         }
-        if( list.size() == 0) {
-            return false;
-        }
-        while( !list.isEmpty() ) {
-            int v = list.remove(list.size()-1);
-            order.add(v);
-            for(int i = 0; i < n; i++) {
-                if( edges[v][i] == 1 ) {
-                    edges[v][i] = 0;
-                    if( helper(i) ) {
-                        list.add(i);
+        while( !q.isEmpty() ) {
+            int course = q.poll();
+            List<Integer> li = map.get(course);
+            if( li != null ) {
+                for(int i = 0; i < li.size(); i++) {
+                    indegree[li.get(i)]--;
+                    if( indegree[li.get(i)] == 0 ) {
+                        q.add(li.get(i));
+                        count++;
                     }
                 }
             }
         }
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if( edges[i][j] == 1) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    
-    public boolean helper(int column) {
-        for(int i = 0; i < n; i++) {
-            if( edges[i][column] == 1) {
-                return false;
-            }
-        }
-        return true;
+        return count == numCourses;
     }
 }
