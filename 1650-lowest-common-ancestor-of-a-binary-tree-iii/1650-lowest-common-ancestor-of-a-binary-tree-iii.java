e443e12e61501c1d3066426293b6ca622fb0aa16
inderSjs <40349728+inderSjs@users.noreply.github.com>
2024-04-22 19:00:36 -0700

    Time: 30 ms (40.77%), Space: 44.7 MB (34.30%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -9,38 +9,16 @@ class Node {
 */
 
 class Solution {
-    private List<Node> pathP;
-    private List<Node> pathQ;
-    
     public Node lowestCommonAncestor(Node p, Node q) {
-        Node root = p;
-        while( root.parent != null) {
-            root = root.parent;
+        HashSet<Node> set = new HashSet<>();
+        Node temp = p;
+        while( temp != null) {
+            set.add(temp);
+            temp = temp.parent;
         }
-        backtrack(root,p,q,new ArrayList<>());
-        for(int i = 0; i < pathP.size(); i++) {
-            if( pathP.get(i) != pathQ.get(i) ) {
-                return pathP.get(i-1);
-            }
+        while( !set.contains(q) ) {
+            q = q.parent;
         }
-        return null;
-    }
-    
-    public void backtrack(Node root, Node p, Node q, List<Node> path) {
-        if( root == null ) {
-            return;
-        }
-        path.add(root);
-        if(root == p) {
-            pathP = new ArrayList<>(path);
-            pathP.add(p);
-        }
-        if(root == q) {
-            pathQ = new ArrayList<>(path);
-            pathQ.add(q);
-        }
-        backtrack(root.left,p,q,path);
-        backtrack(root.right,p,q,path);
-        path.remove(path.size()-1);
+        return q;
     }
 }
