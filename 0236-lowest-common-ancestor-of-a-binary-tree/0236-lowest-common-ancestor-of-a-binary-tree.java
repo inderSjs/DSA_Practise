b5483dd74fda7876bc13abd713b9591f7bdc9820
inderSjs <40349728+inderSjs@users.noreply.github.com>
2024-04-22 18:48:23 -0700

    Time: 12 ms (9.93%), Space: 45.3 MB (10.99%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,48 +8,30 @@
  * }
  */
 class Solution {
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-        // Stack for tree traversal
-        Deque<TreeNode> stack = new ArrayDeque<>();
-
-        // HashMap for parent pointers
-        Map<TreeNode, TreeNode> parent = new HashMap<>();
-
-        parent.put(root, null);
-        stack.push(root);
-
-        // Iterate until we find both the nodes p and q
-        while (!parent.containsKey(p) || !parent.containsKey(q)) {
-
-            TreeNode node = stack.pop();
-
-            // While traversing the tree, keep saving the parent pointers.
-            if (node.left != null) {
-                parent.put(node.left, node);
-                stack.push(node.left);
+        Stack<TreeNode> st = new Stack<>();
+        HashMap<TreeNode, TreeNode> map = new HashMap<>();
+        map.put(root,null);
+        st.push(root);
+        while( !map.containsKey(p) || !map.containsKey(q) ) {
+            TreeNode node = st.pop();
+            if( node.left != null) {
+                st.push(node.left);
+                map.put(node.left, node);
             }
-            if (node.right != null) {
-                parent.put(node.right, node);
-                stack.push(node.right);
+            if( node.right != null) {
+                st.push(node.right);
+                map.put(node.right, node);
             }
         }
-
-        // Ancestors set() for node p.
-        Set<TreeNode> ancestors = new HashSet<>();
-
-        // Process all ancestors for node p using parent pointers.
-        while (p != null) {
-            ancestors.add(p);
-            p = parent.get(p);
+        HashSet<TreeNode> set = new HashSet<>();
+        while( p != null) {
+            set.add(p);
+            p = map.get(p);
         }
-
-        // The first ancestor of q which appears in
-        // p's ancestor set() is their lowest common ancestor.
-        while (!ancestors.contains(q))
-            q = parent.get(q);
+        while( !set.contains(q) ) {
+            q = map.get(q);
+        }
         return q;
     }
-
 }
