a17a0027b1613de2df6c0184ddcf063f8c7a033e
inderSjs <40349728+inderSjs@users.noreply.github.com>
2024-04-22 18:19:57 -0700

    Time: 7 ms (72.55%), Space: 44.9 MB (34.95%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,36 +8,20 @@
  * }
  */
 class Solution {
-    private List<TreeNode> pathP;
-    private List<TreeNode> pathQ;
-    
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        backtrack(root,p,q,new ArrayList<>());
-        for(int i = 0; i < pathP.size(); i++) {
-            if( pathP.get(i) != pathQ.get(i)) {
-                return pathP.get(i-1);
-            }
+        if( root == null || root == p || root == q) {
+            return root;
         }
-        return null;
-    }
-    
-    public void backtrack(TreeNode root, TreeNode p, TreeNode q, List<TreeNode> path) {
-        //base
-        if( root == null ) {
-            return;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if( left == null && right == null) {
+            return null;
+        } else if( left == null && right != null) {
+            return right;
+        } else if( left != null && right == null) {
+            return left;
+        } else {
+            return root;
         }
-        //action
-        path.add(root);
-        if( root == p) {
-            pathP = new ArrayList<>(path);
-            pathP.add(root);
-        }
-        if( root == q) {
-            pathQ = new ArrayList<>(path);
-            pathQ.add(root);
-        }
-        backtrack(root.left,p,q,path);
-        backtrack(root.right,p,q,path);
-        path.remove(path.size()-1);
     }
 }
