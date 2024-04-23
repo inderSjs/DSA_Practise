55596c60d91d73ad74c6bd019229605b8b35fb2c
inderSjs <40349728+inderSjs@users.noreply.github.com>
2024-04-23 16:57:55 -0700

    Time: 8 ms (25.42%), Space: 45.6 MB (17.23%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,40 +1,43 @@
 class Solution {
+    private HashMap<Integer, List<Integer>> map;
+    private boolean[] isVisited;
+    
     public int countComponents(int n, int[][] edges) {
-        boolean[] isVisited = new boolean[n];
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        this.map = new HashMap<>();
+        this.isVisited = new boolean[n];
         for(int[] edge: edges) {
             int v1 = edge[0];
             int v2 = edge[1];
             if( !map.containsKey(v1) ) {
                 map.put(v1, new ArrayList<>());
+                
             }
             if( !map.containsKey(v2) ) {
                 map.put(v2, new ArrayList<>());
+                
             }
             map.get(v1).add(v2);
             map.get(v2).add(v1);
         }
-        Queue<Integer> q = new LinkedList<>();
         int count = 0;
         for(int i = 0; i < n; i++) {
             if( !isVisited[i] ) {
-                q.add(i);
-                while( !q.isEmpty() ) {
-                    int node = q.poll();
-                    isVisited[node] = true;
-                    List<Integer> list = map.get(node);
-                    if( list != null) {
-                        for(int j = 0; j < list.size(); j++) {
-                            int temp = list.get(j);
-                            if( !isVisited[temp] ) {
-                                q.add(temp);
-                            }
-                        }
-                    }
-                }
+                dfs(i);
                 count++;
             }
         }
         return count;
+    }
+    
+    private void dfs(int node) {
+        isVisited[node] = true;
+        List<Integer> list = map.get(node);
+        if( list != null ) {
+            for(int i = 0; i < list.size(); i++) {
+                if( !isVisited[list.get(i)] ) {
+                    dfs(list.get(i));
+                }
+            }
+        }
     }
 }
