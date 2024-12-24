cd4ec16cd4986a0a96d1610705dfa886230fc8c2
inderSjs <40349728+inderSjs@users.noreply.github.com>
2024-12-23 22:59:09 -0800

    Time: 12 ms (98.05%), Space: 49.2 MB (89.34%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.java
+++ 0705-design-hashset/0705-design-hashset.java
@@ -1,79 +1,47 @@
-class Pair<U> {
-    public U ky;
-    
-    public Pair( U ky) {
-        this.ky = ky;
-    }
-}
-
-class Bucket {
-    public List<Pair<Integer>> bucket;
-    
-    public Bucket() {
-        this.bucket = new LinkedList<Pair<Integer>>();
-    }
-    
-    public void update(int key) {
-        boolean bool = false;
-        for(Pair<Integer> pair : this.bucket) {
-            if( pair.ky.equals(key)) {
-                pair.ky = key;
-                bool = true;
-            }
-        }
-        
-        if( !bool ) {
-            Pair<Integer> pa = new Pair<Integer>(key);
-            this.bucket.add(pa);
-        }
-    }
-    
-    public boolean contains_key(int key) {
-        for(Pair<Integer> pair : this.bucket) {
-            if( pair.ky.equals(key)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    public void remove(int key) {
-        for(Pair<Integer> pair : this.bucket) {
-            if( pair.ky.equals(key)) {
-                this.bucket.remove(pair);
-                break;
-            }
-        }
-    }
-    
-}
-
-
 class MyHashSet {
-    public int keySpace;
-    public List<Bucket> set;
+    private boolean[][] storage;
+    private int bucket;
+    private int bucketItem;
 
     public MyHashSet() {
-        this.keySpace = 2069;
-        this.set = new ArrayList<Bucket>();
-        for(int i = 0; i < this.keySpace; i++) {
-            this.set.add(new Bucket());
-        }
+        this.bucket = 1000;
+        this.bucketItem = 1000;
+        this.storage = new boolean[bucket][];
+    }
+    
+    private int hash1(int key) {
+        return key%bucket;
+    }
+    
+    private int hash2(int key) {
+        return key/bucket;
     }
     
     public void add(int key) {
-        int index = key % this.keySpace;
-        this.set.get(index).update(key);
+        int pIdx = hash1(key);
+        int nIdx = hash2(key);
+        if( storage[pIdx] == null) {
+            storage[pIdx] = new boolean[bucketItem+1];
+        }
+        storage[pIdx][nIdx] = true;
     }
     
     public void remove(int key) {
-        int index = key % this.keySpace;
-        this.set.get(index).remove(key);
+        int pIdx = hash1(key);
+        int nIdx = hash2(key);
+        if( storage[pIdx] == null) {
+            return;
+        }
+        storage[pIdx][nIdx] = false;
     }
     
     public boolean contains(int key) {
-        int index = key % this.keySpace;
-        return this.set.get(index).contains_key(key);
+        int pIdx = hash1(key);
+        int nIdx = hash2(key);
+        if( storage[pIdx] == null) {
+            return false;
+        }
+        return storage[pIdx][nIdx];
     }
 }
 
