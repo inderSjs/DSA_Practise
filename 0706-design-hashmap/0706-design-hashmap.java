e665000209b1eebe8042fa1af9ac4459e31f8422
inderSjs <40349728+inderSjs@users.noreply.github.com>
2024-12-24 23:00:06 -0800

    Time: 14 ms (99.58%), Space: 46.7 MB (96.51%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,48 +1,91 @@
 class MyHashMap {
-    private int[][] storage;
-    private int bucket;
-    private int bucketItem;
+    private Node[] storage;
+    
+    class Node {
+        int key;
+        int value;
+        Node next;
+        
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+        
+        public void add(Node n, int key, int value) {
+            Node dummy = n;
+            Node prev = dummy;
+            while( dummy != null ) {
+                if( dummy.key == key) {
+                    dummy.value = value;
+                    return;
+                }
+                prev = dummy;
+                dummy = dummy.next;
+            }
+            prev.next = new Node(key, value);
+        }
+        
+        public void remove(Node n, int key) {
+            Node dummy = n;
+            Node prev = dummy;
+            while( dummy != null ) {
+                if( dummy.key == key) {
+                    break;
+                }
+                prev = dummy;
+                dummy = dummy.next;
+            }
+            if( dummy != null && dummy.next != null ) {
+                prev.next = dummy.next;
+                dummy.next = null;
+            } else {
+                prev.next = null;
+            }
+            
+        }
+        
+        public int get(Node n, int key) {
+            Node temp = n;
+            while( temp != null ) {
+                if( temp.key == key) {
+                    return temp.value;
+                }
+                temp = temp.next;
+            }
+            return -1;
+        }
+    }
 
     public MyHashMap() {
-        this.storage = new int[1000][];
-        this.bucket = 1000;
-        this.bucketItem = 1000;
+        this.storage = new Node[10000];
     }
     
-    private int hash1(int key) {
-        return key%1000;
-    }
-    
-    private int hash2(int key) {
-        return key/1000;
+    private int hash(int key) {
+        return key%10000;
     }
     
     public void put(int key, int value) {
-        int pIdx = hash1(key);
-        int nIdx = hash2(key);
+        int pIdx = hash(key);
         if( storage[pIdx] == null) {
-            storage[pIdx] = new int[bucketItem+1];
-            Arrays.fill(storage[pIdx], -1);
+            storage[pIdx] = new Node(-1,-1);
         }
-        storage[pIdx][nIdx] = value;
+        storage[pIdx].add(storage[pIdx], key, value);
     }
     
     public int get(int key) {
-        int pIdx = hash1(key);
-        int nIdx = hash2(key);
+        int pIdx = hash(key);
         if( storage[pIdx] == null) {
             return -1;
         }
-        return storage[pIdx][nIdx];
+        return storage[pIdx].get(storage[pIdx], key);
     }
     
     public void remove(int key) {
-        int pIdx = hash1(key);
-        int nIdx = hash2(key);
+        int pIdx = hash(key);
         if( storage[pIdx] == null) {
             return;
         }
-        storage[pIdx][nIdx] = -1;
+        storage[pIdx].remove(storage[pIdx], key);
     }
 }
 
